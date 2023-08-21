410cb4b3d861e986bf5f73f9e3467a98923e1188
DESKTOP-PA1UUJO\Admin <karthikeyan.n@agnitio-systems.com>
2023-08-21 20:50:00 +0530

    1.To fix the css issues in adming module-c 2.To change the image cropper syntax code and new function for the latest version plugin-c

--- admin/src/app/shared/shared.module.ts
+++ admin/src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { InlineSVGModule } from 'ng-inline-svg';
 import { ApiService } from './services/api.service';
 import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-//import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 import { AngularEditorModule } from '@kolkov/angular-editor';
 import { ImageCropperModule } from 'ngx-image-cropper';
 import { ControlMessagesComponent } from './components/control-messages/control-messages.component';
