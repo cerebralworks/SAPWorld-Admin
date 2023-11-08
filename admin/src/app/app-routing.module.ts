52ee97c15acf804b7e475cbf0d405dac7e136acc
DESKTOP-PA1UUJO\Admin <karthikeyan.n@agnitio-systems.com>
2023-11-08 14:24:51 +0530

    hashrouter to browser router

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ export const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     scrollPositionRestoration: "enabled",
-    useHash: true
+    useHash: false
 })],
   exports: [RouterModule],
 })
